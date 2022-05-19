e8c7504f9f39c4a94725557cf9e0f1ddcdde348b
DRMADNESS2004 <80173588+DRMADNESS2004@users.noreply.github.com>
2022-05-19 12:04:09 -0400

    simple constructor

--- src/main/java/com/example/final_project_liu_caleb/Score.java
+++ src/main/java/com/example/final_project_liu_caleb/Score.java
@@ -1,4 +1,12 @@
 package com.example.final_project_liu_caleb;
 
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+
 public class Score extends Rectangle implements ICountable{
+    private Label label;
+    public Score(int w, int h, Color c){
+        super(w,h,c);
+
+    }
 }
