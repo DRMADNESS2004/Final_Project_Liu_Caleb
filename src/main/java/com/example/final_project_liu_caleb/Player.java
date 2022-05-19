95e0a2cb293eb28e3b3671e3807cedebffd9a781
DRMADNESS2004 <80173588+DRMADNESS2004@users.noreply.github.com>
2022-05-19 12:01:10 -0400

    constructor and some attributes

--- src/main/java/com/example/final_project_liu_caleb/Player.java
+++ src/main/java/com/example/final_project_liu_caleb/Player.java
@@ -1,4 +1,25 @@
 package com.example.final_project_liu_caleb;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+
 public class Player extends Rectangle{
+    private double posX;
+    private double posY;
+
+    //public Player(double pX, double pY, int w, int h, Color c){
+    //  super(pX,pY,w,h,c);
+    //}
+
+    public Player(double posX, int w, int h, Color c){
+        super(w,h,c);
+        this.posX=posX;
+    }
+
+    //mouse control for player on the left
+    private void movement(Canvas c){
+        c.setOnMouseMoved(e->posY=e.getY());
+    }
+
+
 }
