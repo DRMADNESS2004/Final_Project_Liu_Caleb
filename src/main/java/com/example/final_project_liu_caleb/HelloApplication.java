0cdfc251e7f8ef6bd07e9a17c45e1513ac95402d
DRMADNESS2004 <80173588+DRMADNESS2004@users.noreply.github.com>
2022-05-19 12:04:54 -0400

    started implementing the classes

--- src/main/java/com/example/final_project_liu_caleb/HelloApplication.java
+++ src/main/java/com/example/final_project_liu_caleb/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.final_project_liu_caleb;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage){
-        HBox hb1=new HBox();
-        Scene scene = new Scene(hb1);
+        //Player p1=new Player(0,500,50,100, Color.BLUE);
+        //Player p2=new Player(950,500,50,100, Color.BLUE);
+        Player p1=new Player(0,50,100, Color.BLUE);
+        Player p2=new Player(950,50,100, Color.BLUE);
+        Score s1=new Score(50,50,Color.BLUE);
+        Score s2=new Score(50,50,Color.BLUE);
+
+        Game g1=new Game(800,500,Color.GRAY,p1,p2,s1,s2);
+        Scene scene = g1.getScene();
         stage.setTitle("Ping and Pong");
         stage.setScene(scene);
         stage.show();
