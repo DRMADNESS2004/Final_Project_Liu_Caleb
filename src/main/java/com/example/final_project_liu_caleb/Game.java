14b101fb7c2ec0a31b385d8bdd33e85dfc5a42a1
DRMADNESS2004 <80173588+DRMADNESS2004@users.noreply.github.com>
2022-05-20 14:09:59 -0400

    making the game responsive, ball appearing

--- src/main/java/com/example/final_project_liu_caleb/Game.java
+++ src/main/java/com/example/final_project_liu_caleb/Game.java
@@ -2,12 +2,14 @@ package com.example.final_project_liu_caleb;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import javafx.util.Duration;
 
 public class Game {
@@ -17,27 +19,63 @@ public class Game {
     private Score score1;
     private Score score2;
     private Canvas canvas;
+    private GraphicsContext gc1;
+    public boolean gameStarted=false;
+    private int width;
+    private int height;
 
-    public Game(int w, int h, Color c, Player p1, Player p2, Score s1, Score s2){
-        canvas=new Canvas(w,h);
-        GraphicsContext gc1=canvas.getGraphicsContext2D();
-        gc1.setFill(c);
-        gc1.fillRect(0,0,w,h);
-        //freeform animation defined by one or more keyframes and their duration
-        Timeline t1=new Timeline(new KeyFrame(Duration.millis(10), e->run(gc1)));
-        //defines the number of cycles in animation
-        t1.setCycleCount(Timeline.INDEFINITE);
+    public Game(int w, int h, Color c, Player p1, Player p2, Score s1, Score s2, Circle b){
+        width=w;
+        height=h;
         player1=p1;
         player2=p2;
         score1=s1;
         score2=s2;
+        ball=b;
+    }
+
+    public GraphicsContext getGraph(){
+        return gc1;
+    }
+
+    public Canvas getCanvas() {
+        return canvas;
     }
 
     public Scene getScene(){
         return new Scene(new StackPane(canvas));
     }
 
-    private void run(GraphicsContext gc){
+    //ball and bot movement
+    public void run(GraphicsContext gc){
+        /*gc.setFill(Color.GOLDENROD);
+        gc.fillRect(0,0,800,500);*/
+        if(gameStarted){
+            ball.movement();
 
+            //bot movements
+            if(ball.getPosX()<width-width/4){
+                player2.setPosY(ball.getPosY()-player2.getHeight()/2);
+            }
+            else if(ball.getPosY()>player2.getPosY()+player2.getHeight()/2){
+                player2.setPosY(player2.getPosY()+1);
+            }
+            else{
+                player2.setPosY(player2.getPosY()-1);
+            }
+
+            //draw ball
+            gc.fillOval(ball.getPosX(), ball.getPosY(), ball.getRadius(), ball.getRadius());
+        }
+        else{
+            //set the start menu
+            gc.setStroke(Color.WHITE);
+            gc.setTextAlign(TextAlignment.CENTER);
+            gc.strokeText("Start",width/2, height/2-50);
+            gc.setStroke(Color.WHITE);
+            gc.setTextAlign(TextAlignment.CENTER);
+            gc.strokeText("Settings",width/2, height/2+50);
+
+        }
     }
 }
--- src/main/java/com/example/final_project_liu_caleb/HelloApplication.java
+++ src/main/java/com/example/final_project_liu_caleb/HelloApplication.java
@@ -1,13 +1,16 @@
 package com.example.final_project_liu_caleb;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.util.Duration;
 
 public class HelloApplication extends Application {
     @Override
@@ -18,12 +21,35 @@ public class HelloApplication extends Application {
         Player p2=new Player(950,50,100, Color.BLUE);
         Score s1=new Score(50,50,Color.BLUE);
         Score s2=new Score(50,50,Color.BLUE);
+        Circle c1=new Circle(5,Color.BLACK, 1,1);
 
-        Game g1=new Game(800,500,Color.GRAY,p1,p2,s1,s2);
-        Scene scene = g1.getScene();
+        Game g1=new Game(800,500,Color.GRAY,p1,p2,s1,s2,c1);
+
+
+
+        /*Button start=new Button("Start");
+        start.setOnAction(e->{
+            g1.gameStarted=true;
+        });
+        Button settings=new Button("Settings");
+        settings.setOnAction(e->{
+        });*/
+
+        Canvas canvas=new Canvas(800,500);
+        GraphicsContext gc=canvas.getGraphicsContext2D();
+
+        //freeform animation defined by one or more keyframes and their duration
+        Timeline t1=new Timeline(new KeyFrame(Duration.millis(10), e->g1.run(gc)));
+        //defines the number of cycles in animation
+        t1.setCycleCount(Timeline.INDEFINITE);
+
+        p1.movement(canvas);
+        canvas.setOnMouseClicked(e->g1.gameStarted=true);
+        Scene scene = new Scene(new StackPane(canvas));
         stage.setTitle("Ping and Pong");
         stage.setScene(scene);
         stage.show();
+        t1.play();
     }
 
     public static void main(String[] args) {
