15b151cec2d8d2bb88cff1b9f0a3dbebc6e467dd
DRMADNESS2004 <80173588+DRMADNESS2004@users.noreply.github.com>
2022-05-21 18:17:19 -0400

    working ball mechanics

--- src/main/java/com/example/final_project_liu_caleb/Game.java
+++ src/main/java/com/example/final_project_liu_caleb/Game.java
@@ -9,6 +9,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import javafx.util.Duration;
 
@@ -18,11 +19,12 @@ public class Game {
     private Player player2;
     private Score score1;
     private Score score2;
-    private Canvas canvas;
-    private GraphicsContext gc1;
     public boolean gameStarted=false;
     private int width;
     private int height;
+    private Canvas canvas;
+    private GraphicsContext graphicsContext;
+    private Timeline timeline;
 
     public Game(int w, int h, Color c, Player p1, Player p2, Score s1, Score s2, Circle b){
         width=w;
@@ -32,24 +34,56 @@ public class Game {
         score1=s1;
         score2=s2;
         ball=b;
+        animation();
     }
 
-    public GraphicsContext getGraph(){
-        return gc1;
+    public void animation(){
+        canvas=new Canvas(width,height);
+        graphicsContext=canvas.getGraphicsContext2D();
+        //freeform animation defined by one or more keyframes and their duration
+        timeline=new Timeline(new KeyFrame(Duration.millis(10), e->run(graphicsContext)));
+        //defines the number of cycles in animation
+        timeline.setCycleCount(Timeline.INDEFINITE);
+    }
+
+    public Timeline getTimeline() {
+        return timeline;
     }
 
     public Canvas getCanvas() {
         return canvas;
     }
 
-    public Scene getScene(){
-        return new Scene(new StackPane(canvas));
+    public GraphicsContext getGraphicsContext() {
+        return graphicsContext;
     }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+
 
     //ball and bot movement
     public void run(GraphicsContext gc){
-        /*gc.setFill(Color.GOLDENROD);
-        gc.fillRect(0,0,800,500);*/
+        gc.setFill(Color.GOLDENROD);
+        gc.fillRect(0,0,800,500);
+
+        gc.setFill(Color.PINK);
+        gc.setFont(Font.font(25));
+
         if(gameStarted){
             ball.movement();
 
@@ -76,6 +110,20 @@ public class Game {
             gc.setTextAlign(TextAlignment.CENTER);
             gc.strokeText("Settings",width/2, height/2+50);
 
+            //resets the ball's position and speed
+            ball.setPosY(width/2);
+            ball.setPosY(height/2);
+            ball.setSpeedX(1);
+            ball.setSpeedY(1);
+
+        }
+
+        //makes ball stay in the canvas
+        if(ball.getPosY()+ball.getRadius()>height || ball.getPosY()<0){
+            ball.setSpeedY(ball.getSpeedY()*-1);
+        }
+        else if(ball.getPosX()+ball.getRadius()>width||ball.getPosX()<0){
+            ball.setSpeedX(ball.getSpeedX()*-1);
         }
     }
 }
