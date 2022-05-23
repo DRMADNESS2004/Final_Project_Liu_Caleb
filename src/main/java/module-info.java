88c088d8a3c6b8eaf5bcdb04677786140fbfb7d2
DRMADNESS2004 <80173588+DRMADNESS2004@users.noreply.github.com>
2022-05-22 20:03:56 -0400

    bootstrapfx dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.final_project_liu_caleb {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.kordamp.bootstrapfx.core;
 
 
     opens com.example.final_project_liu_caleb to javafx.fxml;
