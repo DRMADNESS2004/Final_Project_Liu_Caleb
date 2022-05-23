5e622eb2def8855537d9d98b3f06b7453dc9ed40
DRMADNESS2004 <80173588+DRMADNESS2004@users.noreply.github.com>
2022-05-22 20:02:53 -0400

    settings sceen/instances if the settings fields are empty

--- src/main/java/com/example/final_project_liu_caleb/HelloApplication.java
+++ src/main/java/com/example/final_project_liu_caleb/HelloApplication.java
@@ -1,33 +1,80 @@
 package com.example.final_project_liu_caleb;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Duration;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 public class HelloApplication extends Application {
+    private TextField tf1=new TextField();
+    private TextField tf2=new TextField();
+    private TextField tf3=new TextField();
+    private TextField tf4=new TextField();
+    private TextField tf5=new TextField();
+    private TextField tf6=new TextField();
+    private TextField tf7=new TextField();
+    private TextField tf8=new TextField();
+    private TextField tf9=new TextField();
+    private TextField tf10=new TextField();
+    private TextField tf11=new TextField();
+    private TextField tf12=new TextField();
+    private TextField tf13=new TextField();
+    private int width=800;
+    private int height=500;
+
     @Override
     public void start(Stage stage){
-        Player p1=new Player(0,50,100, Color.BLUE);
-        Player p2=new Player(750,50,100, Color.BLUE);
+        Player p1;
+        Player p2;
+        Circle c1;
+        Color color=Color.GRAY;
+        if(!tf1.getText().equals("")){
+            width=Integer.parseInt(tf1.getText());
+        }
+        if(!tf2.getText().equals("")){
+            height=Integer.parseInt(tf2.getText());
+        }
+        if(!tf3.getText().equals("")){
+            color= Color.valueOf(tf3.getText());
+        }
+
+        if(tf8.getText().equals("")||tf9.getText().equals("")||tf10.getText().equals("")){
+            p1=new Player();
+        }
+        else{
+            p1=new Player(0,50,100, Color.BLUE);
+        }
+        if(tf11.getText().equals("")||tf12.getText().equals("")||tf13.getText().equals("")){
+            p2=new Player(750,50,100, Color.BLUE);
+        }
+        else{
+            p2=new Player(width-Integer.parseInt(tf11.getText()),25,100, Color.BLUE);
+        }
+        if(tf4.getText().equals("")||tf5.getText().equals("")||tf6.getText().equals("")||tf7.getText().equals("")){
+            c1=new Circle();
+        }
+        else{
+            c1=new Circle(400,250,50,Color.PINK, 1,1);
+        }
+
         Score s1=new Score(50,50,Color.BLUE);
         Score s2=new Score(50,50,Color.BLUE);
-        Circle c1=new Circle(400,250,50,Color.PINK, 1,1);
 
-        Game g1=new Game(800,500,Color.GRAY,p1,p2,s1,s2,c1);
+        Game g1;
+
+        if(tf1.getText().equals("")||tf2.getText().equals("")||tf3.getText().equals("")){
+            g1=new Game(800,500,Color.GREY,p1,p2,s1,s2,c1);
+        }
+        else{
+            g1=new Game(width,height,color,p1,p2,s1,s2,c1);
+        }
 
         VBox vb1=new VBox(50);
         vb1.setMinWidth(800);
@@ -49,6 +96,7 @@ public class HelloApplication extends Application {
         if(!g1.gameStarted){
             stage.setScene(new Scene(vb1));
         }
+        stage.getScene().getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.show();
         g1.getTimeline().play();
     }
@@ -60,27 +108,56 @@ public class HelloApplication extends Application {
 
     //settings scene
     private Scene settings(){
-        HBox hb1=new HBox();
-        hb1.setMinWidth(800);
-        hb1.setMinHeight(500);
-        TextField tf1=new TextField("Width of canvas");
-        TextField tf2=new TextField("Height of canvas");
-        TextField tf3=new TextField("Color of canvas");
+        VBox vb1=new VBox(50);
+        vb1.setMinWidth(width);
+        vb1.setMinHeight(height);
+        vb1.setAlignment(Pos.CENTER);
 
-        TextField tf4=new TextField("Radius of ball");
-        TextField tf5=new TextField("Color of ball");
-        TextField tf6=new TextField("Speed x of ball");
-        TextField tf7=new TextField("Speed y of ball");
+        VBox vb2=new VBox();
+        vb2.setMaxWidth(width/2);
+        tf1=new TextField();
+        tf1.setPromptText("Width of canvas");
+        tf2=new TextField();
+        tf2.setPromptText("Height of canvas");
+        tf3=new TextField();
+        tf3.setPromptText("Color of canvas");
+        vb2.getChildren().addAll(tf1,tf2,tf3);
 
-        TextField tf8=new TextField("Width of Ping");
-        TextField tf9=new TextField("Height of Ping");
-        TextField tf10=new TextField("Color of Ping");
+        VBox vb3=new VBox();
+        vb3.setMaxWidth(width/2);
+        tf4=new TextField();
+        tf4.setPromptText("Radius of ball");
+        tf5=new TextField();
+        tf5.setPromptText("Color of ball");
+        tf6=new TextField();
+        tf6.setPromptText("Speed x of ball");
+        tf7=new TextField();
+        tf7.setPromptText("Speed y of ball");
+        vb3.getChildren().addAll(tf4,tf5,tf6,tf7);
 
-        TextField tf11=new TextField("Width of Pong");
-        TextField tf12=new TextField("Height of Pong");
-        TextField tf13=new TextField("Color of Pong");
+        VBox vb4=new VBox();
+        vb4.setMaxWidth(width/2);
+        tf8=new TextField();
+        tf8.setPromptText("Width of Ping");
+        tf9=new TextField();
+        tf9.setPromptText("Height of Ping");
+        tf10=new TextField();
+        tf10.setPromptText("Color of Ping");
+        vb4.getChildren().addAll(tf8,tf9,tf10);
 
-        Scene settings=new Scene(hb1);
+        VBox vb5=new VBox();
+        vb5.setMaxWidth(width/2);
+        tf11=new TextField();
+        tf11.setPromptText("Width of Pong");
+        tf12=new TextField();
+        tf12.setPromptText("Height of Pong");
+        tf13=new TextField();
+        tf13.setPromptText("Color of Pong");
+        vb5.getChildren().addAll(tf11,tf12,tf13);
+
+        vb1.getChildren().addAll(vb2,vb3,vb4,vb5);
+
+        Scene settings=new Scene(vb1);
         return settings;
     }
 }
