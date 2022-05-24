d52c978a9fa91e081fdaccd1588d5ac375fe4889
DRMADNESS2004 <80173588+DRMADNESS2004@users.noreply.github.com>
2022-05-24 15:40:44 -0400

    interface method

--- src/main/java/com/example/final_project_liu_caleb/ICountable.java
+++ src/main/java/com/example/final_project_liu_caleb/ICountable.java
@@ -1,7 +1,5 @@
 package com.example.final_project_liu_caleb;
 
-import javafx.scene.control.Label;
-
 public interface ICountable {
-    void compareTo(Label l);
+    String compareTo(Score s);
 }
--- src/main/java/com/example/final_project_liu_caleb/Score.java
+++ src/main/java/com/example/final_project_liu_caleb/Score.java
@@ -2,14 +2,21 @@ package com.example.final_project_liu_caleb;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 
 public class Score extends Rectangle implements ICountable{
+    private HBox hbox=new HBox();
     private Label label=new Label("0");
     private int score;
 
     public Score(int w, int h, Color c){
         super(w,h,c);
+        hbox.setMinWidth(w);
+        hbox.setMinHeight(h);
+        label.setStyle("-fx-background-color:"+c);
     }
 
     public int getScore() {
@@ -26,23 +33,15 @@ public class Score extends Rectangle implements ICountable{
     }
 
     @Override
-    public void compareTo(Label l) {
+    public String compareTo(Score s) {
         int score1=Integer.parseInt(label.getText());
-        int score2=Integer.parseInt(l.getText());
+        int score2=Integer.parseInt(s.getLabel().getText());
 
         if(score1>score2){
-            Alert al1 = new Alert(Alert.AlertType.INFORMATION);
-            al1.setTitle("Information Dialog");
-            al1.setHeaderText("Winner");
-            al1.setContentText("The winner is Ping");
-            al1.showAndWait();
+            return "Ping won!";
         }
-        else if(score1<score2){
-            Alert al1 = new Alert(Alert.AlertType.INFORMATION);
-            al1.setTitle("Information Dialog");
-            al1.setHeaderText("Winner");
-            al1.setContentText("The winner is Pong");
-            al1.showAndWait();
+        else{
+            return "Pong won!";
         }
     }
 }
