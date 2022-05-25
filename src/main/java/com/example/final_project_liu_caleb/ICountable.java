e01f70e9e11e6edf5ae52dc9ead8ff1118b3a73a
DRMADNESS2004 <80173588+DRMADNESS2004@users.noreply.github.com>
2022-05-25 16:16:02 -0400

    comment/removal of the reset method

--- src/main/java/com/example/final_project_liu_caleb/ICountable.java
+++ src/main/java/com/example/final_project_liu_caleb/ICountable.java
@@ -1,7 +1,6 @@
 package com.example.final_project_liu_caleb;
 
+//interface with a method that compares the 2 scores
 public interface ICountable {
     String compareTo(Score s);
-
-    void resetPts(Score s);
 }
